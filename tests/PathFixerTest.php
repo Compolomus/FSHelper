cca32a423857ba2bd9e70e5db2d2d3e92c5afdd0
Paul Rock <paul@drteam.rocks>
2021-05-14 12:54:59 +0300

    PathFixerTest created with 100% coverage

--- tests/PathFixerTest.php
+++ tests/PathFixerTest.php
@@ -2,17 +2,44 @@
 
 declare(strict_types=1);
 
+namespace Compolomus\FSHelper\Tests;
+
+use Compolomus\FSHelper\Exceptions\PathNotFoundException;
 use Compolomus\FSHelper\PathFixer;
 use PHPUnit\Framework\TestCase;
 
 class PathFixerTest extends TestCase
 {
-
-    public function test__construct()
+    public function test__construct(): void
     {
+        $object = new PathFixer('/tmp');
+        self::assertInstanceOf(PathFixer::class, $object);
     }
 
-    public function testFix()
+    public function test__construct_pathNotFoundException(): void
     {
+        $this->expectException(PathNotFoundException::class);
+        new PathFixer('/tmp/path_not_found');
+    }
+
+    public function test_getPath(): void
+    {
+        $object = new PathFixer('/tmp');
+        $test   = $object->getPath();
+        self::assertEquals('/tmp', $test);
+    }
+
+    public function test_getTrimLength(): void
+    {
+        $object = new PathFixer('/tmp');
+        $test   = $object->getTrimLength();
+        self::assertEquals(1, $test);
+    }
+
+    public function test_fix(): void
+    {
+        $object = new PathFixer('/tmp');
+        $test   = $object->fix('/tmp/test');
+        self::assertEquals('tmp/test', $test);
     }
 }
